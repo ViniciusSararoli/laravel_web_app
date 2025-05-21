c24de0f92b9e91399bf23a668225d85202d3ae7b
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-05-21 19:23:34 -0300

    Novos rotas adicionadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreController;
+use App\Http\Controllers\ContatoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/principal', [PrincipalController::class, "principal"]);
+
+Route::get('/sobre', [SobreController::class, "informacao"]);
+
+Route::get('/contato', [ContatoController::class, "numero"]);
